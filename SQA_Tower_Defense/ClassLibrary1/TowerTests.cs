fd96fffdcbeb4825b86f037cec542bdc261e796c
Jackson Melling <mellinja@rose-hulman.edu>
2011-04-07 16:58:27 -0400

    Branch mellinja: tests added

---
+++ SQA_Tower_Defense/ClassLibrary1/TowerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using SQA_Tower_Defense;
+using NUnit.Framework;
+using 
+
+namespace ClassLibrary1
+{
+    [TestFixture()]
+    public class TowerTests
+    {
+
+        [Test()]
+        public void testTowerInit()
+        {
+            Tower t = new Tower();
+            Assert.IsNotNull(t);
+        }
+
+        [Test()]
+        public void testTowerGetsHealth()
+        {
+            Tower t = new Tower();
+            t.setHealth = 10;
+            Assert.AreEqual(t.Health,10);
+
+        }
+
+        [Test()]
+        public void testTowerGetsRange()
+        {
+            Tower t = new Tower();
+            t.setRange = 10;
+            Assert.AreEqual(t.Range, 10);
+
+        }
+
+        [Test()]
+        public void testTowerGetsCost()
+        {
+            Tower t = new Tower();
+            t.setCost = 10;
+            Assert.AreEqual(t.Cost, 10);
+
+        }
+
+        [Test()]
+        public void testTowerGetsAttackDamage()
+        {
+            Tower t = new Tower();
+            t.setAttackDamage = 10;
+            Assert.AreEqual(t.AttackDamage, 10);
+
+        }
+
+        [Test()]
+        public void testTowerGetsEnemy()
+            Tower t = new Tower();
+            
+
+    }
+}
