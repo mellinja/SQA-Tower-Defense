d20187f627920fff2e9f2ff2bbf87815b50e5e36
Brian Baro <baroba@rose-hulman.edu>
2011-05-04 22:10:11 -0700

    Here

---
+++ SQA_Tower_Defense/ClassLibrary1/WaveTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SQA_Tower_Defense;
+using Microsoft.Xna.Framework;
+
+namespace ClassTests
+{
+    [TestFixture()]
+    class WaveTest
+    {
+        Wave wave;
+        [Test]
+        public void WaveInitializes()
+        {
+            Wave wave = new Wave();
+            Assert.IsNotNull(wave); 
+        }
+        [Test]
+        public void WaveHasEnemies()
+        {
+            wave.spawnEnemy(new Enemy (1,1,"basic",1,new Rectangle()); 
+            Assert.AreEqual(wave.EnemyCount(),1); 
+        }
+
+
+
+    }
+}
---
+++ SQA_Tower_Defense/SQA_Tower_Defense/SQA_Tower_Defense/Cell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQA_Tower_Defense
+{
+    public class Cell
+    {
+        Boolean Occupied;
+        Enemy enemy;
+        public Cell()
+        {
+            Occupied = false;
+        }
+
+        public void Occupy(Enemy e)
+        {
+            Occupied = true;
+            enemy = e;
+        }
+
+        public Boolean isOccupied()
+        {
+            return Occupied;
+        }
+
+
+
+    }
+}
